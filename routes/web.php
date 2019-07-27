cdd59111c4d7c6460c4855e3142f744a78d9f33f
nguyenquanglinh2898 <nguyenquanglinh2898@gmail.com>
2019-07-27 16:04:26 +0700

    vietgido

--- routes/web.php
+++ routes/web.php
@@ -22,21 +22,6 @@ Route::get('model/select-all',function(){
     echo "</pre>";
 });
 
-<<<<<<< HEAD
-// Route::get('model/select-id',function(){
-//     $data = App\Film::find(24)->toArray();
-//     echo "<pre>";
-//     print_r($data);
-//     echo "</pre>";
-// });
-
-// Route::get('db',function(){
-//     $data = DB::table('ticket','film','schedule')
-//     ->select(DB::raw('ticket.id as idTicket, schedule.film_id as idFilm'))
-//     ->where('ticket.schedule_id','schedule.id');
-//     echo ($data);
-// });
-=======
 Route::get('trangChu','indexController@index');
 //Route::get('thayDoiMatKhau','UserController@thayDoiMatKhau');
->>>>>>> f0033c22aa98278d2884772b47d5325e8f767a93
+//>>>>>>> f0033c22aa98278d2884772b47d5325e8f767a93
