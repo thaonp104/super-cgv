a418143ff6e4fa4f6fdcee7a9823a721821accc3
N.P Thao <nguyenphuongthao12041998@gmail.com>
2019-08-04 16:14:37 +0700

    detail and mua ve

---
+++ app/Cinema.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cinema extends Model
+{
+    protected $table = 'cinema';
+    public $timestamps = false;
+}
---
+++ app/Member.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model{
+    protected $table = 'member';
+    public $timestamps = false;
+}
+
+
