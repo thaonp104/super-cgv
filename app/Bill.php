5fbe057f75db5eb77f55eaeb0620f98865d7d485
nguyenquanglinh2898 <nguyenquanglinh2898@gmail.com>
2019-08-08 20:44:44 +0700

    sửa conflict

--- app/Bill.php
+++ app/Bill.php
@@ -7,11 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Bill extends Model
 {
     protected $table = 'bill';
-<<<<<<< HEAD
 
     protected $fillable = ['payment_date','payment_type','quantity','client_id','member_id'];
 
-=======
->>>>>>> b2ac90f8fbd9c5ada2ec670ca57a3a57d688b76c
     public $timestamps = false;
 }
