df8ac5e2f3a923dced3f56958045e054e2b61c94
nguyenquanglinh2898 <nguyenquanglinh2898@gmail.com>
2019-08-20 02:28:32 +0700

    thêm lịch chiếu

--- routes/web.php
+++ routes/web.php
@@ -20,37 +20,6 @@ Route::get('/phimdangchieu','PhimDangChieuController@index');
 Route::get('/','indexController@index');
 
 Route::get('/detailFilm/{id}','indexController@detailFilm');
-Route::get('/cinemaa','indexController@cinema');
-
-Route::get('/cinema', function () {
-    
-    // Các phòng chiếu phim đã chọn
-    // $room = App\Film::find(2)->room()->get()->toArray();
-    
-    // // Thời gian chiếu phim đã chọn
-    // $time = [];
-    // $j = 0;
-    // foreach ($room as $each_room) {
-    //     $time[$j++] = App\Film::find($each_room['id'])->schedule()->get()->toArray();
-    // }
-
-    // // Các rạp chiếu phim đã chọn
-    // $cinema = [];
-    // $i = 0;
-    // foreach ($room as $each_room) {
-    //     $cinema[$i++] = App\Room::find($each_room['id'])->cinema()->get()->toArray();
-    // }
-
-    echo "<pre>";
-    print_r($time);
-    echo "</pre><br><br>";
-    echo "<pre>";
-    print_r($room);
-    echo "</pre><br><br>";
-    echo "<pre>";
-    print_r($cinema);
-    echo "</pre>";
-});
 
 Route::get('/login','indexController@login');
 
