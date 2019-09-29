e82d5b5482dbf5a0e8dbc73b22b053411b1c6538
N.P Thao <nguyenphuongthao12041998@gmail.com>
2019-09-29 23:58:19 +0700

    link to booking and update transaction history

--- resources/views/MyAccount/transactionHistory.blade.php
+++ resources/views/MyAccount/transactionHistory.blade.php
@@ -31,66 +31,102 @@
                         </li>
                     </ul>
                 </div>
+                <div class="links">
+                    {{$bills->links()}}
+                </div>
                 <div class="transactions">
                     @foreach($bills as $bill)
-                        @foreach($tickets as $ticket)
-                            @if($bill->id == $ticket->bill_id)
-                                <div class="transaction">
-                                    <p class="transaction_id">
-                                        Mã Đặt Vé: {{$bill->id}}
-                                        <span>(Trạng thái: Hoàn Tất)</span>
-                                        <i class="fa fa-television"></i>
-                                    </p>
-                                    <div class="transaction_content">
-                                        <div class="film_image">
-                                            @foreach($tickets as $ticket)
-                                                @if($bill->id == $ticket->bill_id)
-                                                    @foreach($schedules as $schedule)
-                                                        @if($schedule->id == $ticket->schedule_id)
-                                                            @foreach($films as $film)
-                                                                @if($film->id == $schedule->film_id)
-                                                                    <img src="{{URL::asset($film->image)}}">
-                                                                    @break
-                                                                @endif
-                                                            @endforeach
+                        <div class="transaction">
+                            <p class="transaction_id">
+                                Mã Đặt Vé: {{$bill->id}}
+                                <span>(Trạng thái: Hoàn Tất)</span>
+                                <i class="fa fa-television"></i>
+                            </p>
+                            <div class="transaction_content">
+                                <div class="film_image">
+                                    @foreach($tickets as $ticket)
+                                        @if($bill->id == $ticket->bill_id)
+                                            @foreach($schedules as $schedule)
+                                                @if($schedule->id == $ticket->schedule_id)
+                                                    @foreach($films as $film)
+                                                        @if($film->id == $schedule->film_id)
+                                                            <img src="{{URL::asset($film->image)}}">
                                                             @break
                                                         @endif
                                                     @endforeach
                                                     @break
                                                 @endif
                                             @endforeach
+                                            @break
+                                        @endif
+                                    @endforeach
 
-                                        </div>
-                                        <div class="film_infor">
-                                            <p class="film_title">
-                                                @foreach($tickets as $ticket)
-                                                    @if($bill->id == $ticket->bill_id)
-                                                        @foreach($schedules as $schedule)
-                                                            @if($schedule->id == $ticket->schedule_id)
-                                                                @foreach($films as $film)
-                                                                    @if($film->id == $schedule->film_id)
-                                                                        <?php
-                                                                        $str = $film->name;
-                                                                        $str = strtoupper($str);
-                                                                        echo $str;
-                                                                        ?>
-                                                                        @break
-                                                                    @endif
-                                                                @endforeach
+                                </div>
+                                <div class="film_infor">
+                                    <p class="film_title">
+                                        @foreach($tickets as $ticket)
+                                            @if($bill->id == $ticket->bill_id)
+                                                @foreach($schedules as $schedule)
+                                                    @if($schedule->id == $ticket->schedule_id)
+                                                        @foreach($films as $film)
+                                                            @if($film->id == $schedule->film_id)
+                                                                <?php
+                                                                $str = $film->name;
+                                                                $str = strtoupper($str);
+                                                                echo $str;
+                                                                ?>
                                                                 @break
                                                             @endif
                                                         @endforeach
                                                         @break
                                                     @endif
                                                 @endforeach
-                                            </p>
-                                            @foreach($tickets as $ticket)
-                                                @if($bill->id == $ticket->bill_id)
-                                                    @foreach($schedules as $schedule)
-                                                        @if($schedule->id == $ticket->schedule_id)
-                                                            @foreach($films as $film)
-                                                                @if($film->id == $schedule->film_id)
-                                                                    <p class="movie_rating {{$film->rated}}"></p>
+                                                @break
+                                            @endif
+                                        @endforeach
+                                    </p>
+                                    @foreach($tickets as $ticket)
+                                        @if($bill->id == $ticket->bill_id)
+                                            @foreach($schedules as $schedule)
+                                                @if($schedule->id == $ticket->schedule_id)
+                                                    @foreach($films as $film)
+                                                        @if($film->id == $schedule->film_id)
+                                                            <p class="movie_rating {{$film->rated}}"></p>
+                                                            @break
+                                                        @endif
+                                                    @endforeach
+                                                    @break
+                                                @endif
+                                            @endforeach
+                                            @break
+                                        @endif
+                                    @endforeach
+                                    @foreach($tickets as $ticket)
+                                        @if($bill->id == $ticket->bill_id)
+                                            @foreach($schedules as $schedule)
+                                                @if($schedule->id == $ticket->schedule_id)
+                                                    @foreach($films as $film)
+                                                        @if($film->id == $schedule->film_id)
+                                                            <p>{{$film->release_date}}</p>
+                                                            @break
+                                                        @endif
+                                                    @endforeach
+                                                    @break
+                                                @endif
+                                            @endforeach
+                                            @break
+                                        @endif
+                                    @endforeach
+                                    <p>From 22:00 PM ~ To 00:19 AM</p>
+                                    @foreach($tickets as $ticket)
+                                        @if($bill->id == $ticket->bill_id)
+                                            @foreach($schedules as $schedule)
+                                                @if($schedule->id == $ticket->schedule_id)
+                                                    @foreach($rooms as $room)
+                                                        @if($room->id == $schedule->room_id)
+                                                            @foreach($cinemas as $cinema)
+                                                                @if($cinema->id == $room->cinema_id)
+                                                                    <p>{{$cinema->name}}</p>
                                                                     @break
                                                                 @endif
                                                             @endforeach
@@ -100,72 +136,34 @@
                                                     @break
                                                 @endif
                                             @endforeach
-                                            @foreach($tickets as $ticket)
-                                                @if($bill->id == $ticket->bill_id)
-                                                    @foreach($schedules as $schedule)
-                                                        @if($schedule->id == $ticket->schedule_id)
-                                                            @foreach($films as $film)
-                                                                @if($film->id == $schedule->film_id)
-                                                                    <p>{{$film->release_date}}</p>
-                                                                    @break
-                                                                @endif
-                                                            @endforeach
-                                                            @break
-                                                        @endif
-                                                    @endforeach
-                                                    @break
-                                                @endif
-                                            @endforeach
-                                            <p>From 22:00 PM ~ To 00:19 AM</p>
-                                            @foreach($tickets as $ticket)
-                                                @if($bill->id == $ticket->bill_id)
-                                                    @foreach($schedules as $schedule)
-                                                        @if($schedule->id == $ticket->schedule_id)
-                                                            @foreach($rooms as $room)
-                                                                @if($room->id == $schedule->room_id)
-                                                                    @foreach($cinemas as $cinema)
-                                                                        @if($cinema->id == $room->cinema_id)
-                                                                            <p>{{$cinema->name}}</p>
-                                                                            @break
-                                                                        @endif
-                                                                    @endforeach
-                                                                    @break
-                                                                @endif
-                                                            @endforeach
-                                                            @break
-                                                        @endif
-                                                    @endforeach
-                                                    @break
-                                                @endif
-                                            @endforeach
-                                            <p>
-                                                @foreach($tickets as $ticket)
-                                                    @if($bill->id == $ticket->bill_id)
-                                                        @foreach($seats as $seat)
-                                                            @if($seat->id == $ticket->seat_id)
-                                                                {{$seat->name}}
-                                                            @endif
-                                                        @endforeach
-                                                        @break
+                                            @break
+                                        @endif
+                                    @endforeach
+                                    <p>
+                                        @foreach($tickets as $ticket)
+                                            @if($bill->id == $ticket->bill_id)
+                                                @foreach($seats as $seat)
+                                                    @if($seat->id == $ticket->seat_id)
+                                                        {{$seat->name}}
                                                     @endif
                                                 @endforeach
-                                            </p>
-                                            <p class="transaction_price">
-                                                <?php $price=0; ?>
-                                                @foreach($tickets as $ticket)
-                                                    @if($bill->id == $ticket->bill_id)
-                                                        <?php  $price = $price+ $ticket->price; ?>
-                                                    @endif
-                                                @endforeach
-                                                {{$price}}
-                                            </p>
-                                            <button class="transaction_detail"><a href="#">Xem</a></button>
-                                        </div>
-                                    </div>
+                                            @endif
+                                        @endforeach
+                                    </p>
+                                    <p class="transaction_price">
+                                        <?php $price=0; ?>
+                                        @foreach($tickets as $ticket)
+                                            @if($bill->id == $ticket->bill_id)
+                                                <?php  $price = $price+ $ticket->price; ?>
+                                            @endif
+                                        @endforeach
+                                        {{$price}}
+                                    </p>
+                                    <button class="transaction_detail"><a href="#">Xem</a></button>
                                 </div>
-                                <hr>
-                            @endif
-                        @endforeach
+                            </div>
+                        </div>
+                        <hr>
 
                     @endforeach
                 </div>
